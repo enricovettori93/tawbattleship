25c4f1dafca9ead14d0d176e678f50b605501b91
Enrico Vettori <enrico.vettori93@gmail.com>
2018-05-17 14:36:02 +0200

    Added service Utilities

--- client/battleship/src/app/scoreboard/scoreboard.component.ts
+++ client/battleship/src/app/scoreboard/scoreboard.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse } from '@angular
 import { UserService } from '../user.service';
 import { Observable } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
+import { UtilitiesService } from '../utilities.service';
 
 @Component({
   selector: 'app-scoreboard',
@@ -11,19 +12,7 @@ import { tap, catchError } from 'rxjs/operators';
 })
 export class ScoreboardComponent implements OnInit {
   private scoreboard = [];
-  constructor(private http: HttpClient, private userService: UserService) { }
-
-  private create_options(params = {}) {
-    console.log("PARAMS QUERY " + JSON.stringify(params))
-    return {
-      headers: new HttpHeaders({
-        authorization: 'Bearer ' + this.userService.get_token(),
-        'cache-control': 'no-cache',
-        'Content-Type': 'application/json',
-      }),
-      params: new HttpParams({ fromObject: params })
-    };
-  }
+  constructor(private http: HttpClient, private userService: UserService, private utilities: UtilitiesService) { }
 
   ngOnInit() {
     this.getScoreboard().subscribe((scoreboard) => {
@@ -36,7 +25,7 @@ export class ScoreboardComponent implements OnInit {
   }
 
   getScoreboard(params = {}):Observable<any>{
-    return this.http.get('http://localhost:8080/scoreboard',this.create_options(params)).pipe(
+    return this.http.get('http://localhost:8080/scoreboard',this.utilities.create_options(this.userService.get_token(),params)).pipe(
       tap((data) => {
         console.log(JSON.stringify(data));
       })
