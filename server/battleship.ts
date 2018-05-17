6f962b9069bf5a3997a5f2df28a731809598d6ea
Enrico Vettori <enrico.vettori93@gmail.com>
2018-05-17 12:29:37 +0200

    Visualizzazione utenti, admin modificano attributo admin di altri

--- server/battleship.ts
+++ server/battleship.ts
@@ -19,9 +19,9 @@ import mongoose = require('mongoose');
 
 import { User } from './User';
 import * as user from './User';
-import {Message} from './Message';
+import { Message } from './Message';
 import * as message from './Message';
-import {Chat} from './Chat';
+import { Chat } from './Chat';
 import * as chat from './Chat';
 
 import express = require('express');
@@ -97,7 +97,7 @@ app.get("/login", passport.authenticate("basic", { session: false }), (req, res,
 app.route("/users/:username").get(auth, (req, res, next) => {
     console.log("Called endpoint user info with username: " + req.params.username);
     //Get /users/:username information
-    user.getModel().findOne({ username: req.params.username }, { salt: 0, digest: 0 , _id: 0 , __v: 0}).then((user) => {
+    user.getModel().findOne({ username: req.params.username }, { salt: 0, digest: 0, _id: 0, __v: 0 }).then((user) => {
         return res.status(200).json(user);
     }).catch((reason) => {
         return next({ statusCode: 404, error: true, errormessage: "Error get user info: " + reason });
@@ -108,21 +108,32 @@ app.route("/users/:username").get(auth, (req, res, next) => {
     if (!user.newUser(req.user).hasAdminRole() && user.newUser(req.user).username != req.params.username) {
         return next({ statusCode: 404, error: true, errormessage: "Unauthorized" });
     }
-    //Creo un user dummy al volo per calcolare digest e salt da inserire nell'utente da modificare
-    var userdummy = user.newUser({
-        name: "",
-        surname: "",
-        username: "",
-        mail: ""
-    });
-    userdummy.setPassword(req.body.password);
-    //TODO: manca da gestire la possibilità di diventare admin se un Admin lo concede aggiornando l'utente
-    user.getModel().updateOne({ username: req.params.username }, { "$set": { "username": req.body.username, "name": req.body.name, "surname": req.body.surname, "mail": req.body.mail, "salt": userdummy.salt, "digest": userdummy.digest} }).then(() => {
-        console.log(("User with username " + req.params.username + " updated").yellow);
-        return res.status(200).json({ error: false, errormessage: "" });
-    }).catch((error) => {
-        return next({ statuscode: 404, error: true, errormessage: "MongoDB error: " + error });
-    })
+    if (req.body.password === undefined) {
+        //Utente admin non modifica i dati dell'altro utente, ma solo l'attributo isAdmin
+        user.getModel().updateOne({ username: req.body.username }, { "$set": {"isAdmin": req.body.isAdmin } }).then(() => {
+            console.log(("User with username " + req.body.username + " updated").yellow);
+            return res.status(200).json({ error: false, errormessage: "" });
+        }).catch((error) => {
+            return next({ statuscode: 404, error: true, errormessage: "MongoDB error: " + error });
+        })
+    }
+    else {
+        //Creo un user dummy al volo per calcolare digest e salt da inserire nell'utente da modificare
+        var userdummy = user.newUser({
+            name: "",
+            surname: "",
+            username: "",
+            mail: ""
+        });
+        userdummy.setPassword(req.body.password);
+        //TODO: manca da gestire la possibilità di diventare admin se un Admin lo concede aggiornando l'utente
+        user.getModel().updateOne({ username: req.params.username }, { "$set": { "username": req.body.username, "name": req.body.name, "surname": req.body.surname, "mail": req.body.mail, "salt": userdummy.salt, "digest": userdummy.digest, "isAdmin": req.body.isAdmin } }).then(() => {
+            console.log(("User with username " + req.params.username + " updated").yellow);
+            return res.status(200).json({ error: false, errormessage: "" });
+        }).catch((error) => {
+            return next({ statuscode: 404, error: true, errormessage: "MongoDB error: " + error });
+        })
+    }
 }).delete(auth, (req, res, next) => {
     //Delete /users/:username
     if (!user.newUser(req.user).hasAdminRole() && user.newUser(req.user).username != req.params.username) {
@@ -141,8 +152,8 @@ app.route("/users").post((req, res, next) => {
     if (!req.body.password) {
         return next({ statusCode: 500, error: true, errormessage: "Missing password" });
     }
-    new_user.partitePerse =  0;
-    new_user.partiteVinte =  0;
+    new_user.partitePerse = 0;
+    new_user.partiteVinte = 0;
     new_user.setPassword(req.body.password);
     new_user.isAdmin = false;
     new_user.save().then((data) => {
@@ -156,7 +167,7 @@ app.route("/users").post((req, res, next) => {
 }).get(auth, (req, res, next) => {
     if (req.query.keysearched === undefined) {
         console.log("Called endpoint user search without word");
-        user.getModel().find({},{ salt: 0, digest: 0, _id: 0, __v: 0, chatList: 0 }).then((documents) => {
+        user.getModel().find({}, { salt: 0, digest: 0, _id: 0, __v: 0, chatList: 0 }).then((documents) => {
             return res.status(200).json(documents);
         }).catch((error) => {
             return next({ statusCode: 404, error: true, errormessage: "MongoDB error: " + error })
@@ -178,12 +189,12 @@ app.route("/users").post((req, res, next) => {
 app.route("/chats").get(auth, (req, res, next) => {
     //Get all chats of auth user
     console.log(("Getting " + req.user.username + " chats").blue);
-    user.getModel().find({username: req.user.username},{"chatList":1, "_id": 0}).then((documents) => {
+    user.getModel().find({ username: req.user.username }, { "chatList": 1, "_id": 0 }).then((documents) => {
         return res.status(200).json(documents);
     }).catch((error) => {
-        return next({statusCode: 404, error: true, errormessage: "MongoDB error: " + error});
+        return next({ statusCode: 404, error: true, errormessage: "MongoDB error: " + error });
     })
-}).post(auth, (req,res, next) => {
+}).post(auth, (req, res, next) => {
     var dest = req.body.destinatario;
     var id_dest = undefined;
     var id_sender = undefined;
@@ -191,13 +202,13 @@ app.route("/chats").get(auth, (req, res, next) => {
 
     console.log(("Creating chat between " + req.user.username + " and " + dest).green);
 
-    var query_id_dest = user.getModel().findOne({username: dest});
-    var query_id_sender = user.getModel().findOne({username: req.user.username});
+    var query_id_dest = user.getModel().findOne({ username: dest });
+    var query_id_sender = user.getModel().findOne({ username: req.user.username });
 
     var promise_query_id_dest = query_id_dest.exec();
     var promise_query_id_sender = query_id_sender.exec();
 
-    Promise.all([promise_query_id_dest,promise_query_id_sender]).then(values => {
+    Promise.all([promise_query_id_dest, promise_query_id_sender]).then(values => {
         id_dest = values[0]._id;
         id_sender = values[1]._id;
         var new_chat = chat.newChat({
@@ -209,44 +220,44 @@ app.route("/chats").get(auth, (req, res, next) => {
         new_chat.save().then((data) => {
             console.log(("Chat between " + req.user.username + " and " + dest + " created").green);
             //Aggiorno la lista delle chat dei 2 utenti con quella apena creata
-            var promise1 = user.getModel().update({_id: id_sender},{$push: {chatList: data._id}});
-            var promise2 = user.getModel().update({_id: id_dest},{$push: {chatList: data._id}});
-            Promise.all([promise1, promise2]).then(function() {
+            var promise1 = user.getModel().update({ _id: id_sender }, { $push: { chatList: data._id } });
+            var promise2 = user.getModel().update({ _id: id_dest }, { $push: { chatList: data._id } });
+            Promise.all([promise1, promise2]).then(function () {
                 console.log(("Chat addedd succesfully").green);
-                return res.status(200).json({error: false, errormessage: ""});
-            }).catch(function(error) {
+                return res.status(200).json({ error: false, errormessage: "" });
+            }).catch(function (error) {
                 console.log("MongoDB error saving chats: " + error);
-                return next({statusCode: 404, error: true, errormessage: "MongoDB error: " + error});
+                return next({ statusCode: 404, error: true, errormessage: "MongoDB error: " + error });
             });
         }).catch((error) => {
-            if(error.code === 11000)
-                return next({statusCode: 404, error: true, errormessage: "Chat already exists"});
-            return next({statusCode: 404, error: true, errormessage: "MongoDB error: " + error});
+            if (error.code === 11000)
+                return next({ statusCode: 404, error: true, errormessage: "Chat already exists" });
+            return next({ statusCode: 404, error: true, errormessage: "MongoDB error: " + error });
         })
     }).catch((error) => {
-        return next({statusCode: 404, error: true, errormessage: "MongoDB error retrieving ids for new chat: " + error});
+        return next({ statusCode: 404, error: true, errormessage: "MongoDB error retrieving ids for new chat: " + error });
     });
-}).delete(auth, (req,res,next) => {
+}).delete(auth, (req, res, next) => {
     //Endpoint di prova, cancella la chat con parametro destinatario nel body
     console.log(("Deleting chat between " + req.user.username + " and " + req.body.destinatario).zebra);
 
-    var query_id_dest = user.getModel().findOne({username: req.body.destinatario});
-    var query_id_sender = user.getModel().findOne({username: req.user.username});
+    var query_id_dest = user.getModel().findOne({ username: req.body.destinatario });
+    var query_id_sender = user.getModel().findOne({ username: req.user.username });
 
     var promise_query_id_dest = query_id_dest.exec();
     var promise_query_id_sender = query_id_sender.exec();
 
-    Promise.all([promise_query_id_sender,promise_query_id_dest]).then(values => {
-        var query_get_id_chat  = chat.getModel().findOne({user1ID: values[0]._id,user2ID: values[1]._id});
-        var query_delete_chat = chat.getModel().deleteOne({user1ID: values[0]._id,user2ID: values[1]._id});
-        
+    Promise.all([promise_query_id_sender, promise_query_id_dest]).then(values => {
+        var query_get_id_chat = chat.getModel().findOne({ user1ID: values[0]._id, user2ID: values[1]._id });
+        var query_delete_chat = chat.getModel().deleteOne({ user1ID: values[0]._id, user2ID: values[1]._id });
+
         var promise_query_get_id_chat = query_get_id_chat.exec();
         var promise_query_delete_chat = query_delete_chat.exec();
 
-        Promise.all([promise_query_get_id_chat,promise_query_delete_chat]).then(values2 => {
-            user.getModel().update({chatList: values2[0]._id},{$set: {chatList: values2[0]._id, val: undefined}});
+        Promise.all([promise_query_get_id_chat, promise_query_delete_chat]).then(values2 => {
+            user.getModel().update({ chatList: values2[0]._id }, { $set: { chatList: values2[0]._id, val: undefined } });
         }).catch((error) => {
-            return next({statusCode: 404, error: true, errormessage: "MongoDB error: " + error});
+            return next({ statusCode: 404, error: true, errormessage: "MongoDB error: " + error });
         })
 
         /*
@@ -261,13 +272,13 @@ app.route("/chats").get(auth, (req, res, next) => {
 })
 
 //---------------------- Scoreboard Endpoints ----------------------
-app.get("/scoreboard", (req,res,next) => {
-    req.query.limit = parseInt( req.query.limit || "10" ) || 10;
+app.get("/scoreboard", (req, res, next) => {
+    req.query.limit = parseInt(req.query.limit || "10") || 10;
     console.log(("Printing scoreboard with limit: " + req.query.limit).magenta);
-    user.getModel().find({},{"username": 1, "partiteVinte": 1, "_id": 0}).sort({partiteVinte: -1}).limit(req.query.limit).then((documents) => {
+    user.getModel().find({}, { "username": 1, "partiteVinte": 1, "_id": 0 }).sort({ partiteVinte: -1 }).limit(req.query.limit).then((documents) => {
         return res.status(200).json(documents);
     }).catch((error) => {
-        return next({statusCode: 404, error: true, errormessage: "MongoDB error: " + error});
+        return next({ statusCode: 404, error: true, errormessage: "MongoDB error: " + error });
     })
 })
 
