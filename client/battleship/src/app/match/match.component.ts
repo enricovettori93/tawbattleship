b577633a73074d63a2f6e868ccbdf7e18fa41b7c
Unknown <veneri.alberto@gmail.com>
2018-06-01 08:37:51 +0200

    Compile error fix for match component

--- client/battleship/src/app/match/match.component.ts
+++ client/battleship/src/app/match/match.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../user.service';
-import { UtilitiesService } from '../utilities.service';
-import { MatchService } from '../match.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../user.service";
+import { UtilitiesService } from "../utilities.service";
+import { MatchService } from "../match.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-match",
@@ -12,24 +12,24 @@ import { Router } from '@angular/router';
 export class MatchComponent implements OnInit {
   private userName: string;
   private userMatches: string;
-  private matchService: MatchService;
 
   private match;
   private id_match;
 
-  constructor(private userService: UserService, private utilities: UtilitiesService, private matchService: MatchService, private router: Router) { }
+  constructor(private userService: UserService, private utilities: UtilitiesService,
+    private matchService: MatchService, private router: Router) { }
 
   ngOnInit() {
     this.utilities.check_auth(this.userService.get_token());
-    this.id_match = (this.router.url.split('/'))[2];
+    this.id_match = (this.router.url.split("/"))[2];
     this.getSingleMatch(this.id_match);
   }
 
-  getSingleMatch(id: string){
+  getSingleMatch(id: string) {
     this.matchService.getSingleMatch(id).subscribe((match) => {
       this.match = match;
-    }), (err) => {
+    }, (err) => {
       console.log(JSON.stringify(err));
-    }
+    });
   }
 }
