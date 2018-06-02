84dc6252a5a853fa687fc43eec152e5ae7e8bc73
Unknown <veneri.alberto@gmail.com>
2018-06-02 14:35:06 +0200

    Linter fix

--- client/battleship/src/app/list-matches/list-matches.component.spec.ts
+++ client/battleship/src/app/list-matches/list-matches.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ListMatchesComponent } from './list-matches.component';
+import { ListMatchesComponent } from "./list-matches.component";
 
-describe('ListMatchesComponent', () => {
+describe("ListMatchesComponent", () => {
   let component: ListMatchesComponent;
   let fixture: ComponentFixture<ListMatchesComponent>;
 
@@ -19,7 +19,7 @@ describe('ListMatchesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
