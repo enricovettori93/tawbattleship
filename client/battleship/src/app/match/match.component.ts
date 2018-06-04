468f15e8ac9ed8b8b0ade98175a36ea8d4a8b26f
Unknown <veneri.alberto@gmail.com>
2018-06-04 11:07:18 +0200

    Intial version of table version of match component

--- client/battleship/src/app/match/match.component.ts
+++ client/battleship/src/app/match/match.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
 import { UserService } from "../user.service";
 import { UtilitiesService } from "../utilities.service";
 import { MatchService } from "../match.service";
@@ -10,10 +10,12 @@ import { Router } from "@angular/router";
   templateUrl: "./match.component.html",
   styleUrls: ["./match.component.css"]
 })
-export class MatchComponent implements OnInit {
+export class MatchComponent implements OnInit, AfterViewInit {
+  @ViewChild("battlefieldDOM") battlefieldDOM: ElementRef;
   private userName: string;
   private userMatches: string;
-
+  columns: Array<string>;
+  rows: Array<string>;
   private match;
   private id_match;
 
@@ -27,14 +29,71 @@ export class MatchComponent implements OnInit {
   ngOnInit() {
     this.utilities.check_auth(this.userService.get_token());
     this.id_match = (this.router.url.split("/"))[2];
-    this.socketIoService.connect(this.id_match).subscribe((data) => {
-      this.matchService.getSingleMatch(this.id_match).subscribe((match) => {
-        this.match = match;
-      }, (err) => {
-        console.log(JSON.stringify(err));
-      });
+    this.columns = Array(10).fill(0).map((x, i) => (String.fromCharCode(97 + i)));
+    this.rows = Array(10).fill(0).map((x, i) => (i + ""));
+    /** TODO
+     * this.socketIoService.connect(this.id_match).subscribe((data) => {
+
     }
-    );
+    );*/
+    this.matchService.getSingleMatch(this.id_match).subscribe((match) => {
+      this.match = match;
+    }, (err) => {
+      console.log(JSON.stringify(err));
+    });
+  }
+  ngAfterViewInit() {
+    document.addEventListener("DOMContentLoaded", App.init);
+}
+
+}
+
+class App {
+  static box;
+  static className;
+  static init() {
+
+    App.box = document.getElementsByClassName("box")[0];
+
+    App.box.addEventListener("dragstart", App.dragstart);
+    App.box.addEventListener("dragend", App.dragend);
+
+    const containers = document.getElementsByClassName("holder");
+
+    for (const container of containers) {
+      container.addEventListener("dragover", App.dragover);
+      container.addEventListener("dragenter", App.dragenter);
+      container.addEventListener("dragleave", App.dragleave);
+      container.addEventListener("drop", App.drop);
+    }
+  }
+
+  static dragstart() {
+    this.className += " held";
+
+    setTimeout(() => this.className = "invisible", 0);
+  }
+
+  static dragend() {
+    this.className = "box";
+  }
+
+  static dragover(e) {
+    e.preventDefault();
+  }
+
+  static dragenter(e) {
+    e.preventDefault();
+    this.className += " hovered";
+  }
+
+  static dragleave() {
+    this.className = "holder";
+  }
+
+  static drop() {
+    this.className = "holder";
+    this.append(App.box);
   }
 
 }
