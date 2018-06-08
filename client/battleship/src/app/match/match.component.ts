576b5ce9844f2bd4e6870e9208548339842a0928
Unknown <veneri.alberto@gmail.com>
2018-06-08 07:57:25 +0200

    Match page init

--- client/battleship/src/app/match/match.component.ts
+++ client/battleship/src/app/match/match.component.ts
@@ -12,7 +12,11 @@ import { MatchService } from '../match.service';
 })
 export class MatchComponent implements OnInit {
   matchId: string;
-  match;
+  private match;
+  opponentUsr: string;
+  userBoard;
+  userShip;
+  opponentBoard;
 
   constructor(private userService: UserService,
     private router: Router,
@@ -25,12 +29,16 @@ export class MatchComponent implements OnInit {
     this.utilities.check_auth(this.userService.get_token());
     this.activatedRoute.paramMap.subscribe((data) => {
       this.matchId = data.get('id');
-      this.matchService.getSingleMatch(this.matchId).subscribe(
+      this.matchService.getMatchInfo(this.matchId, true).subscribe(
         (match) => {
           console.log(match);
+          this.match = match;
+          this.opponentUsr = match.opponentInfo.username;
+          this.userBoard = match.myBoard.matrix;
+          this.opponentBoard = match.opponentBoard.matrix;
         }
       );
-    });
-  }
+  });
+}
 
 }
